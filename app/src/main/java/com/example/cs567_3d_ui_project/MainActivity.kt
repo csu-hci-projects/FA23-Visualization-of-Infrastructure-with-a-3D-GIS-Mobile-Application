798408a4bb57728d3c62631bd9900f7cb65f2a40
rmartinez2 <rmartinez@knights.ucf.edu>
2023-10-15 11:52:37 -0600

    Burger Menu with Option Appears!
    activity_main.xml
    -Implemented main as DrawerLayout
    -Added an ID for the drawer layout itself
    
    MainActivity.kt
    -Commented out the tool bar approach with the ellipsis
    -Re-enabled the code used to interact with the drawer layout.
    
    themes.xml
    -Put the default toolbar back onto the theme to support the drawer layout.

--- app/src/main/java/com/example/cs567_3d_ui_project/MainActivity.kt
+++ app/src/main/java/com/example/cs567_3d_ui_project/MainActivity.kt
@@ -2,47 +2,48 @@ package com.example.cs567_3d_ui_project
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.cs567_3d_ui_project.ui.theme.CS567_3D_UI_ProjectTheme
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var toolBar: Toolbar
-//    private lateinit var navigationView: NavigationView
-//    private lateinit var drawerToggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
+//    private lateinit var toolBar: Toolbar
+//    private lateinit var navigationView: NavigationView
+//    private lateinit var navController: NavController
+//    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try{
             setContentView(R.layout.activity_main)
-            val navHostFragment = supportFragmentManager.findFragmentById(
-                R.id.nav_host_container) as NavHostFragment
+//            val navHostFragment = supportFragmentManager.findFragmentById(
+//                R.id.nav_host_container) as NavHostFragment
 
-//            drawerLayout = findViewById(R.id.drawer_layout)
+            drawerLayout = findViewById(R.id.main_drawer_layout)
+            drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close)
 
-            toolBar = findViewById(R.id.toolbar)
-            navController = navHostFragment.navController
-            appBarConfiguration = AppBarConfiguration(navController.graph)
+            drawerLayout.addDrawerListener(drawerToggle)
+            drawerToggle.syncState()
+
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+//            toolBar = findViewById(R.id.toolbar)
+//            navController = navHostFragment.navController
+//            appBarConfiguration = AppBarConfiguration(navController.graph)
             //findViewById<NavigationView>(R.id.navigationView).setupWithNavController(navController)
-            toolBar.setupWithNavController(navController, appBarConfiguration)
-            toolBar.inflateMenu(R.menu.drawer_view)
+//            toolBar.setupWithNavController(navController, appBarConfiguration)
+//            toolBar.inflateMenu(R.menu.drawer_view)
 
 //
 //            drawerLayout = findViewById(R.id.drawer_layout)
@@ -66,14 +67,18 @@ class MainActivity : AppCompatActivity() {
 //    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+//        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.drawer_view, menu)
-        return true
-//        return super.onCreateOptionsMenu(menu)
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.drawer_view, menu)
+//        return true
+////        return super.onCreateOptionsMenu(menu)
+//    }
 
 
     private fun showError(message: String) {
