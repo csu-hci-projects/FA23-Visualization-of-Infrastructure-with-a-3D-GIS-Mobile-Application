bb2a87547a65606a7c0c59634e4e240bd668f0e0
rmartinez2 <rmartinez@knights.ucf.edu>
2023-11-10 11:05:46 -0700

    Progress in Creating the Base Code for Creating 3D Objects

---
+++ app/src/main/java/com/example/cs567_3d_ui_project/argis/geometry/objects/GeneratedData.kt
@@ -0,0 +1,7 @@
+package com.example.cs567_3d_ui_project.argis.geometry.objects
+
+import com.example.cs567_3d_ui_project.argis.geometry.interfaces.DrawCommand
+
+class GeneratedData(val vertexData: FloatArray, val drawList: List<DrawCommand>) {
+
+}
