1deeb1cfcf1e151f610f0fe704328a1132c01bb6
rmartinez2 <rmartinez@knights.ucf.edu>
2023-11-15 19:56:26 -0700

    An Options Button to Signify A Place to Select an Action for the User

--- app/src/main/java/com/example/cs567_3d_ui_project/argis/renderers/ARGISRenderer.kt
+++ app/src/main/java/com/example/cs567_3d_ui_project/argis/renderers/ARGISRenderer.kt
@@ -365,14 +365,3 @@ data class WrappedAnchor(
     val trackable: Trackable
 )
 
-//fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?){
-//    GLES20.glClearColor(1.0f, 1.0f, 0.4f, 0.4f)
-//}
-//
-//fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
-//    GLES20.glViewport(0,0,width,height)
-//}
-//
-//override fun onDrawFrame(p0: GL10?) {
-//    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-//}
--- app/src/main/java/com/example/cs567_3d_ui_project/views/ARGISView.kt
+++ app/src/main/java/com/example/cs567_3d_ui_project/views/ARGISView.kt
@@ -3,6 +3,9 @@ package com.example.cs567_3d_ui_project.views
 import android.opengl.GLSurfaceView
 import android.util.Log
 import android.view.View
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import com.example.cs567_3d_ui_project.R
@@ -12,6 +15,22 @@ class ARGISView(val activity: ARGISActivity): DefaultLifecycleObserver {
 
     val root = View.inflate(activity, R.layout.argis_view, null)
     val surfaceView = root.findViewById<GLSurfaceView>(R.id.surfaceview)
+
+    val optionsMenuButton = root.findViewById<ImageButton>(R.id.options).apply {
+        setOnClickListener {
+            v -> PopupMenu(activity, v).apply {
+                setOnMenuItemClickListener {
+                    item ->
+                    when(item.itemId){
+                        R.id.edit -> Toast.makeText(activity.applicationContext, "Navigate/Display Edit Buttons Next", Toast.LENGTH_LONG)
+                        else -> null
+                    } != null
+                }
+            inflate(R.menu.ar_options_menu)
+            show()
+            }
+        }
+    }
 
     val session
         get() = activity.arGISSessionHelper.mySession
